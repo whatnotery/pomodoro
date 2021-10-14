e7fffd823caeaecad747ee6657c0859fae40347e
Jos O'shea <jos.oshea.ky@gmail.com>
2021-10-13 21:25:36 -0400

    added Bulma

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let name;
+</script>
+
+<style global lang="scss">
+	@import "main.scss";
+</style>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<button class="button is-primary">Button</button>
+</main>
+
